5b5b248b77a33ea5c9577da277673772b3a5aba1
Nischita Pai <nvp15@pitt.edu>
2021-10-06 21:40:21 -0400

    adding initial copies of AS2

---
+++ Assignment2/src/Indexing/PreProcessedCorpusReader.py
@@ -0,0 +1,10 @@
+import Classes.Path as Path
+
+class PreprocessedCorpusReader:
+
+    def __init__(self, type):
+        return
+
+    # Read a line for docNo from the corpus, read another line for the content, and return them in [docNo, content].
+    def nextDocument(self):
+        return
