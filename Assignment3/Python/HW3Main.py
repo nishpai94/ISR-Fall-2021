85e78a2f2eb5b4bba6f243fc565f08673fe0af8f
Nischita Pai <nvp15@pitt.edu>
2021-10-27 00:51:48 -0400

    Adding initial AS3

---
+++ Assignment3/Python/HW3Main.py
@@ -0,0 +1,21 @@
+import IndexingWithWhoosh.MyIndexReader as MyIndexReader
+import Search.QueryRetreivalModel as QueryRetreivalModel
+import Search.ExtractQuery as ExtractQuery
+import datetime
+
+
+startTime = datetime.datetime.now()
+
+index = MyIndexReader.MyIndexReader("trectext")
+search = QueryRetreivalModel.QueryRetrievalModel(index)
+extractor = ExtractQuery.ExtractQuery()
+queries= extractor.getQuries()
+for query in queries:
+    print(query.topicId,"\t",query.queryContent)
+    results = search.retrieveQuery(query, 20)
+    rank = 1
+    for result in results:
+        print(query.getTopicId()," Q0 ",result.getDocNo(),' ',rank," ",result.getScore()," MYRUN",)
+
+endTime = datetime.datetime.now()
+print ("load index & retrieve the token running time: ", endTime - startTime)
